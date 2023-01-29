93271edac665a29d41ff9808b9d8bb6f5367f082
Shahriddinov <shahriddinovmurod195@gmail.com>
2023-01-29 12:00:33 +0500

    responsive

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../../assets//images/logo.svg";
+import burger from "../../../assets/images/icons/burger-mob.svg";
+
 const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<div className="header">
 			<Link to="/">
 				<img className="logo" src={Logo} alt="" />
 			</Link>
-			<navbar className="navbar">
+			<div className="burger-mob" onClick={() => setIsOpen(true)}>
+						<img src={!isOpen ? burger : ""} alt="mobile burger" />
+					</div>
+					{isOpen ?  <div>
+						<navbar className="navbar">
 				<li>
 					<ul>
 						<a href="/">Hem</a>{" "}
@@ -15,12 +22,17 @@ const Header = () => {
 				</li>
 				<li>
 					<ul>
-						<a href="/about">Search Guide</a>{" "}
+						<a href="/about">Sökguide</a>{" "}
 					</ul>
 				</li>
 				<li>
 					<ul>
-						<a href="/search">About Us</a>
+						<a href="/search">Priser</a>
+					</ul>
+				</li>
+				<li>
+					<ul>
+						<a href="/search">Om oss</a>
 					</ul>
 				</li>
 				<li>
@@ -30,6 +42,8 @@ const Header = () => {
 				</li>
 			</navbar>
 			<button className="login">Logga in</button>
+					</div> : ''}
+			
 		</div>
 	);
 };
